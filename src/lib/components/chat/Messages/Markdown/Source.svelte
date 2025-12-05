e9b5a302f94aa33fc07dc3d37f7d7126fb3c931a
DidO <77dido@github.com>
2025-12-05 18:14:20 +0100

    fix: harmonisation du style des citations en jaune (badges, bordure, liste)

--- src/lib/components/chat/Messages/Markdown/Source.svelte
+++ src/lib/components/chat/Messages/Markdown/Source.svelte
@@ -37,7 +37,7 @@
 
 {#if title !== 'N/A'}
 	<button
-		class="text-[10px] w-fit translate-y-[2px] px-2 py-0.5 dark:bg-white/5 dark:text-white/80 dark:hover:text-white bg-gray-50 text-black/80 hover:text-black transition rounded-xl"
+		class="text-[10px] w-fit translate-y-[2px] px-2 py-0.5 dark:bg-yellow-900/20 dark:text-yellow-200 dark:hover:text-yellow-100 bg-yellow-50 text-yellow-800 hover:text-yellow-900 transition rounded-xl"
 		on:click={() => {
 			onClick(id);
 		}}
